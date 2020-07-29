af4bdaa014a45ad223d006229b8462d04b1fd432
shivasaitechatcore <shivasai@techatcore.com>
2020-07-29 16:40:13 +0530

    react

---
+++ Hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Hello() {
+  return <h1> Hello tech At Core</h1>
+}
+export default Hello
---
+++ first.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+function Greet( ) {
+   return <h1> function component</h1>
+}
+export default Greet;
+
+
